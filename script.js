393ce7bdfa3e59556a2cbec02691dd449e380cd5
quan0311 <quan031125@gmail.com>
2024-10-30 20:23:55 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,3 @@
-document.getElementById('introButton').addEventListener('click', function() {
-    const intro = document.getElementById('intro');
-    if (intro.style.display === 'none') {
-        intro.style.display = 'block';
-        this.textContent = 'Ẩn giới thiệu';
-    } else {
-        intro.style.display = 'none';
-        this.textContent = 'Xem giới thiệu';
-    }
-});
+function openLink(url) {
+    window.open(url, '_blank');
+}
