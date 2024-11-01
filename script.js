72452a659f2b55254c499170c333bfb20d4d5141
quan0311 <quan031125@gmail.com>
2024-11-01 12:18:46 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
-function openLink(url) {
-    window.open(url, '_blank');
-}
+// JavaScript để hiển thị thông báo khi nhấp vào .container
+document.querySelector('.container').addEventListener('click', () => {
+    alert("Chào bạn! Đây là trang giới thiệu bản thân của Quân.");
+});
